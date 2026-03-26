0eecfc9f3bc99b0058b69c0f6a98c1ecd70fc18a
Johan Gabriel <johan.universidade@gmail.com>
2026-03-26 20:29:52 -0300

    Rest realizado

--- fornecedores/src/main/java/com/johan/fornecedores/Controller/FornecedorController.java
+++ fornecedores/src/main/java/com/johan/fornecedores/Controller/FornecedorController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/fornecedores")
+@RequestMapping(path = "/fornecedores")
 public class FornecedorController {
 
     @Autowired
